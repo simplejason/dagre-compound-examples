f53d4a5dd07cca12cc005233fad9f3c97aeadd27
simplejason <simplejason.coder@gmail.com>
2021-01-07 19:57:33 +0800

    fix(react): create components for single parts

--- react-compound-examples/src/App.tsx
+++ react-compound-examples/src/App.tsx
@@ -1,21 +1,186 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
-import React from 'react';
+import {
+  buildGraph,
+  HierarchyGraphNodeInfo,
+  HierarchyGraphDef,
+  HierarchyBaseEdgeInfo,
+  HierarchyBaseNodeInfo
+} from 'dagre-compound';
+import * as d3 from 'd3';
+
+interface SvgContainerProps {
+  source: HierarchyGraphDef;
+}
+
+interface SvgContainerState {
+  renderInfo: HierarchyGraphNodeInfo;
+}
+
+interface GroupNodeProps {
+  renderInfo: HierarchyGraphNodeInfo;
+  type: 'root' | 'group';
+}
+
+interface GraphEdgeProps {
+  edge: HierarchyBaseEdgeInfo;
+}
+
+interface GraphNodeProps {
+  node: HierarchyGraphNodeInfo | HierarchyBaseNodeInfo;
+}
+
+const line = d3
+  .line<{ x: number; y: number }>()
+  .x(d => d.x)
+  .y(d => d.y)
+  .curve(d3.curveBasis);
+
+export class SvgContainer extends React.Component<SvgContainerProps, SvgContainerState> {
+  constructor(props: SvgContainerProps) {
+    super(props);
+    const { source } = this.props;
+    this.state = {
+      renderInfo: buildGraph(source)
+    };
+  }
+
+  expand = (): void => {
+    const { source } = this.props;
+    this.setState({
+      renderInfo: buildGraph(source, {
+        expanded: ['GROUP0']
+      })
+    })
+  }
+
+  collapse = (): void => {
+    const { source } = this.props;
+    this.setState({
+      renderInfo: buildGraph(source, {
+        expanded: []
+      })
+    })
+  }
+
+  render() {
+    console.log(this.state.renderInfo)
+    return (
+        <div className="graph-container">
+          <div className="buttons">
+            <button onClick={this.expand}>Expand</button>
+            <button onClick={this.collapse}>Collapse</button>
+          </div>
+
+          <svg className="graph">
+            <g className="zoom">
+              <GroupNode renderInfo={this.state.renderInfo} type={'root'} />
+            </g>
+          </svg>
+        </div>
+    );
+  }
+}
+
+// Group node
+export class GroupNode extends React.Component<GroupNodeProps> {
+  subGraphTransform = (node: HierarchyGraphNodeInfo) => {
+    const x = node.x - node.width / 2.0 + node.paddingLeft;
+    const y = node.y - node.height / 2.0 + node.paddingTop;
+    return `translate(${x}, ${y})`;
+  };
+
+  coreTransform = (node: HierarchyGraphNodeInfo) => {
+    return `translate(0, ${node.labelHeight})`;
+  };
+
+  render() {
+    const { type, renderInfo } = this.props;
+    const edges = renderInfo.edges.map((edge)=>
+      <GraphEdge key={edge.v+'--'+edge.w} edge={edge} />
+    );
+    const nodes = renderInfo.nodes.map((node)=>
+        <GraphNode key={node.name} node={node} />
+    );
+    return (
+      <g className={type} transform={type === 'group' ? this.subGraphTransform(renderInfo) : undefined}>
+        <g className="core" transform={this.coreTransform(renderInfo)}>
+          <g className="edges">
+            {edges}
+          </g>
+          <g className="nodes">
+            {nodes}
+          </g>
+        </g>
+      </g>
+    );
+  }
+}
+
+// Group node
+export class GraphEdge extends React.Component<GraphEdgeProps> {
+  render() {
+    const { edge } = this.props;
+    const points = line(edge.points)!;
+    return (
+      <g className="edge" key={edge.v + '--' + edge.w}>
+        <path className="edge-line" data-edge={edge.v + '--' + edge.w} d={points} />
+      </g>
+    );
+  }
+}
+
+// Group node
+export class GraphNode extends React.Component<GraphNodeProps> {
+   computeCXPositionOfNodeShape = (
+      node: HierarchyBaseNodeInfo | HierarchyGraphNodeInfo
+  ): number => {
+    if ((node as HierarchyGraphNodeInfo).expanded) {
+      return node.x;
+    }
+    const dx = 0;
+    return node.x - node.width / 2 + dx + node.coreBox.width / 2;
+  };
+   nodeTransform = (node: HierarchyBaseNodeInfo | HierarchyGraphNodeInfo) => {
+    const x = this.computeCXPositionOfNodeShape(node) - node.width / 2;
+    const y = node.y - node.height / 2;
+    return `translate(${x}, ${y})`;
+  };
+
+  render() {
+    const { node } = this.props;
+    return (
+      <g className={node.type === 2 ? 'node bridge' : 'node'} key={node.name}>
+        <g transform={this.nodeTransform(node)}>
+          <rect width={node.width} height={node.height} className="node-rect" />
+          <text>
+            <tspan dy="1em" x="1">
+              {node.name}
+            </tspan>
+          </text>
+        </g>
+        {
+          (node as HierarchyGraphNodeInfo).expanded &&
+              <GroupNode renderInfo={node as HierarchyGraphNodeInfo} type={'group'} />
+        }
+      </g>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const data = {
+    nodes: [{ id: '0-1' }, { id: '0-2' }, { id: '1-1' }, { id: '1-2' }],
+    edges: [
+      { v: '0-1', w: '0-2' },
+      { v: '0-2', w: '1-1' },
+      { v: '1-1', w: '1-2' }
+    ],
+    compound: {
+      GROUP0: ['0-1', '0-2']
+    }
+  };
+  return <SvgContainer source={data} />;
 }
 
 export default App;
